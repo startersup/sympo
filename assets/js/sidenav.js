235f70995d38cedb52d72eed39a1ecd53125658e
saicharan96 <saicharan14996@gmail.com>
2019-02-04 21:05:09 +0530

    sss
    sss

--- assets/js/sidenav.js
+++ assets/js/sidenav.js
@@ -7,28 +7,3 @@ $(function () {
 
 
 
-function openNav() {
-  document.getElementById("mySidebar").style.width = "180px";
-  document.getElementById("main").style.marginLeft = "180px";
-}
-
-function closeNav() {
-  document.getElementById("mySidebar").style.width = "0";
-  document.getElementById("main").style.marginLeft= "0";
-}
-
-
-$(window).on('load', function() { // makes sure the whole site is loaded 
-  $('#status').fadeOut(); // will first fade out the loading animation 
-  $('#preloader').delay(300).fadeOut('slow'); // will fade out the white DIV that covers the website. 
-  $('load').delay(300).css({'overflow':'visible'});
-})
-
-	// Prevent dropdown menu from closing when click inside the form
-	$(document).on("click", ".navbar-right .dropdown-menu", function(e){
-		e.stopPropagation();
-	});
-
-
-
-
