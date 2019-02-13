b4a439a3d3fa730820c924a452d3320407fe8ca2
saicharan96 <saicharan14996@gmail.com>
2019-02-14 01:18:15 +0530

    ss
    sss

--- PaytmKit/TxnTest.php
+++ PaytmKit/TxnTest.php
@@ -4,65 +4,67 @@
 	header("Expires: 0");
 
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-<html>
+<!DOCTYPE html>
+<html lang="en">
+
 <head>
-<title>Merchant Check Out Page</title>
-<meta name="GENERATOR" content="Evrsoft First Page">
+    <meta charset="utf-8">
+    <title>Petrovision | National Level Technical Symposium</title>
+    <link rel="icon" href="../assets/images/logo.png" type="image" sizes="16x16">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://fonts.googleapis.com/css?family=Merriweather|Open+Sans|Roboto:500" rel="stylesheet">
+    <link rel="stylesheet" href="../assets/css/style.css">
+    <script src="../assets/js/sidenav.js"></script>
+    <link href="https://fonts.googleapis.com/css?family=Archivo+Black|Asap|Bangers|Carter+One|Concert+One|Francois+One|Kanit|Merienda|Noto+Sans+TC|Anton|Passion+One|Signika|Vollkorn" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="../assets/css/fonts.css">
+    <script src="../assets/js/sidenav.js"></script>
 </head>
-<body>
-	<h1>Merchant Check Out Page</h1>
-	<pre>
-	</pre>
-	<form method="post" action="pgRedirect.php">
-		<table border="1">
-			<tbody>
-				<tr>
-					<th>S.No</th>
-					<th>Label</th>
-					<th>Value</th>
-				</tr>
-				<tr>
-					<td>1</td>
-					<td><label>ORDER_ID::*</label></td>
-					<td><input id="ORDER_ID" tabindex="1" maxlength="20" size="20"
-						name="ORDER_ID" autocomplete="off"
-						value="<?php echo  "ORDS" . rand(10000,99999999)?>">
-					</td>
-				</tr>
-				<tr>
-					<td>2</td>
-					<td><label>CUSTID ::*</label></td>
-					<td><input id="CUST_ID" tabindex="2" maxlength="12" size="12" name="CUST_ID" autocomplete="off" value="CUST001"></td>
-				</tr>
-				<tr>
-					<td>3</td>
-					<td><label>INDUSTRY_TYPE_ID ::*</label></td>
-					<td><input id="INDUSTRY_TYPE_ID" tabindex="4" maxlength="12" size="12" name="INDUSTRY_TYPE_ID" autocomplete="off" value="Retail"></td>
-				</tr>
-				<tr>
-					<td>4</td>
-					<td><label>Channel ::*</label></td>
-					<td><input id="CHANNEL_ID" tabindex="4" maxlength="12"
-						size="12" name="CHANNEL_ID" autocomplete="off" value="WEB">
-					</td>
-				</tr>
-				<tr>
-					<td>5</td>
-					<td><label>txnAmount*</label></td>
-					<td><input title="TXN_AMOUNT" tabindex="10"
-						type="text" name="TXN_AMOUNT"
-						value="1">
-					</td>
-				</tr>
-				<tr>
-					<td></td>
-					<td></td>
-					<td><input value="CheckOut" type="submit"	onclick=""></td>
-				</tr>
-			</tbody>
-		</table>
-		* - Mandatory Fields
-	</form>
-</body>
-</html>
+    <body style="background-color:#000000;opacity:0.8;">
+     <nav class="navbar navbar-default navit navbar-fixed-top">
+        <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+           <center> <a class="navbar-brand" href="#">
+               <span> <img class="logo" src="../assets/images/logo.png">Petrovision</span>
+            </a></center>
+        </div>
+        <div class="collapse navbar-collapse " id="myNavbar">
+
+               <ul class="nav navbar-nav navbar-right " id="myTopnav">
+                <li><a href="../index.php">Home</a>
+                </li>
+                <li><a href="../about/">About</a>
+                </li>
+                <li><a href="../events/">Events</a>
+                </li>
+                <li><a href="../workshop/">Workshops</a>
+                </li>
+                <li><a href="../sponsor/">Sponsors</a>
+                </li>
+                <li><a href="../accomodation/">Accomodation</a>
+                </li>
+                <li><a href="../contact/">Contact</a>
+                </li>
+                <li><a href="../gallery/">Gallery</a></li>
+
+                <?php
+                session_start();
+                  if(isset($_SESSION['id'])){
+                  echo "<li><a href='/logout/'>Logout</a>
+               </li> ";
+             } else {
+               echo "<li><a href='/login/'>Login</a>
+            </li>";
+             }       ?>
+            </ul>
+        </div>
+
+        </nav></body></html>
