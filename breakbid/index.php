ad6ebb41e8c73356f3f6b25d4f7a664767cd1387
saicharan96 <saicharan14996@gmail.com>
2019-02-14 01:05:08 +0530

    Update index.php
    ss

--- breakbid/index.php
+++ breakbid/index.php
@@ -215,7 +215,7 @@
   border: 0;
   margin-right: 0;
    font-family: 'Anton', sans-serif;
-     font-size:17px;
+     font-size:18px;
     letter-spacing: 1.9px;
     line-height:29px;
     font-weight:400;
