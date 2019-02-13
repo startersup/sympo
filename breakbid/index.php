567c96290e072e09e85d4c14f1fe5212bd487e26
saicharan96 <saicharan14996@gmail.com>
2019-02-13 23:49:09 +0530

    Update index.php
    sss

--- breakbid/index.php
+++ breakbid/index.php
@@ -247,6 +247,20 @@
             {
                 background-color:transparent;
             }
+            @media (max-width: 425px)
+{
+
+    .tabbable-line > .nav-tabs > li > a {
+    border: 0;
+    margin-right: 0;
+    font-family: 'Anton', sans-serif;
+    font-size: 14px;
+    letter-spacing: 1.9px;
+    line-height: 29px;
+    font-weight: 400;
+    margin: 0px 0px;
+    color: #737373;
+    }}
 </style>
 
 
