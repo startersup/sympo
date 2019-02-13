cad715e7f78d8e28016754131d68109beeae52a1
saicharan96 <saicharan14996@gmail.com>
2019-02-14 01:03:17 +0530

    Update index.php
    sss

--- breakbid/index.php
+++ breakbid/index.php
@@ -214,11 +214,17 @@
 .tabbable-line > .nav-tabs > li > a {
   border: 0;
   margin-right: 0;
-    font-size:25px;
+   font-family: 'Anton', sans-serif;
+     font-size:17px;
+    letter-spacing: 1.9px;
+    line-height:29px;
+    font-weight:400;
+    margin:0px 10px;
   color: #737373;
 }
 .tabbable-line > .nav-tabs > li > a > i {
   color: #a6a6a6;
+    
 }
 .tabbable-line > .nav-tabs > li.open, .tabbable-line > .nav-tabs > li:hover {
   border-bottom: 4px solid #fbcdcf;
