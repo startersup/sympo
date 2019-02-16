4671f21fd088979be7161a55a73d7a435ae1c792
saicharan96 <saicharan14996@gmail.com>
2019-02-16 14:19:31 +0530

    Update index.php
    ddd

--- index.php
+++ index.php
@@ -98,6 +98,13 @@
 
             </div>
         </section>
+                 <footer class=""><center><a href="https://www.facebook.com/PetroVisionOfficial/" class="fa fa-facebook"></a>
+<a href="#" class="fa fa-twitter"></a>
+<a href="https://instagram.com/petrovision2019?utm_source=ig_profile_share&igshid=1g99nkx7wd9pd" class="fa fa-instagram"></a>
+</center>
+     <br><center><p>Copyrights &copy 2019 Petrovision. All Rights Reserved</p>
+        <span class="craft">Crafted By <a href="http://www.seotechie.tk/" target="_blank">SEOTechie</a></span></center>
+        </footer>
             </div>
 </section>
         
