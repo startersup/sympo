151561f7cbdc38b64413e4acafb57d9939dc02a0
vinodhan16 <vinodhan.thunderbolt@gmail.com>
2019-02-17 12:23:19 +0530

    Update config_paytm.php

--- PaytmKit/lib/config_paytm.php
+++ PaytmKit/lib/config_paytm.php
@@ -7,9 +7,9 @@
 - Above details will be different for testing and production environment.
 */
 
-define('PAYTM_ENVIRONMENT', 'TEST'); // PROD
-define('PAYTM_MERCHANT_KEY', 'zLzSpd90x0!BlbBr'); //Change this constant's value with Merchant key received from Paytm.
-define('PAYTM_MERCHANT_MID', 'zoVZGC10424999031613'); //Change this constant's value with MID (Merchant ID) received from Paytm.
+define('PAYTM_ENVIRONMENT', 'LIVE'); // PROD
+define('PAYTM_MERCHANT_KEY', 'fNh%0DcOzZBzRqhs'); //Change this constant's value with Merchant key received from Paytm.
+define('PAYTM_MERCHANT_MID', 'nXAQQu67838400396182'); //Change this constant's value with MID (Merchant ID) received from Paytm.
 define('PAYTM_MERCHANT_WEBSITE', 'WEBSTAGING'); //Change this constant's value with Website name received from Paytm.
 
 $PAYTM_STATUS_QUERY_NEW_URL='https://securegw-stage.paytm.in/merchant-status/getTxnStatus';
