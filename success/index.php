eaf932a3796b023454f9d6081d6bcad79609279c
saicharan96 <saicharan14996@gmail.com>
2019-02-20 20:54:36 +0530

    Update index.php
    ss

--- success/index.php
+++ success/index.php
@@ -191,7 +191,8 @@ $headers .= "From: Petrovision <info@petrovision.in> \r\n";
 $headers .= "Reply-To: Petrovision <info@petrovision.in> \r\n"."X-Mailer: PHP/";// . phpversion();
 $headers .= "MIME-Version: 1.0" . "\r\n";
 $headers .= "Content-type:text/html;charset=UTF-8" . "\r\b";
-    mail('saicharan14996@gmail.com', 'Greetings from Petrovision','<body link="#00a5b5" vlink="#00a5b5" alink="#00a5b5">
+    mail('saicharan14996@gmail.com', 'Greetings from Petrovision',$message,implode("\r\n", $headers));
+    $message = '<body link="#00a5b5" vlink="#00a5b5" alink="#00a5b5">
 
 <table class=" main contenttable" align="center" style="font-weight: normal;border-collapse: collapse;border: 0;margin-left: auto;margin-right: auto;padding: 0;font-family: Arial, sans-serif;color: #555559;background-color: white;font-size: 16px;line-height: 26px;width: 600px;">
 		<tr>
@@ -309,8 +310,7 @@ We, the organisers of Petrovision 2k19 would like to welcome you to our Extravag
             font-size:18px !important;
         }
 
-	}</style>',implode("\r\n", $headers));
-    $values = '' ;
+	}</style>' ;
 
  
 }
