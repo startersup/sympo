b3cf658f3c7895b959331932e323fdccb817dd7a
saicharan96 <saicharan14996@gmail.com>
2019-02-24 10:14:40 +0530

    Update index.php
    ss

--- workshop/index.php
+++ workshop/index.php
@@ -66,6 +66,7 @@
     <a href="#" class="close" data-dismiss="alert" aria-label="close" style="color:black !important;">&times;</a>
    We are working Hard to provide you the Best Workshops in our Symposium
   </div></center>
+                <br><br><br>
         </div></div>
         <footer class=""><center><a href="https://www.facebook.com/PetroVisionOfficial/" class="fa fa-facebook"></a>
        <a href="#" class="fa fa-twitter"></a>
