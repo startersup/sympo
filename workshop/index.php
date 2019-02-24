fa6693912a4eca9d07bb9474113724204b0fd4cc
saicharan96 <saicharan14996@gmail.com>
2019-02-24 10:11:05 +0530

    Update index.php
    ss

--- workshop/index.php
+++ workshop/index.php
@@ -62,9 +62,9 @@
         <div class="top evento">
             <div class="container">
             <div class="logos"><b style="font-family: 'Anton', sans-serif;font-style:20px;margin-top:-40px;">C<span>om</span>ing<span> S</span>oon !</b></div>
-        <center><p style=" font-family: 'ProximaNovaSemibold';color:#ffffff;font-size:20px;">We are working Hard to provide you the Best Workshops in our Symposium</p></center></div>
+        <center><p style=" font-family: 'ProximaNovaSemibold';color:#ffffff;font-size:20px;">We are working Hard to provide you the Best Workshops in our Symposium</p></center>
         <br><br><br><br>
-        <hr></div>
+        <hr></div></div>
         <footer class=""><center><a href="https://www.facebook.com/PetroVisionOfficial/" class="fa fa-facebook"></a>
        <a href="#" class="fa fa-twitter"></a>
        <a href="https://www.instagram.com/_petrovision2k19q" class="fa fa-instagram"></a>
