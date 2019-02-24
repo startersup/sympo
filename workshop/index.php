9a96c19cdc34c86529cd6ead11bdfa3be63f81b5
saicharan96 <saicharan14996@gmail.com>
2019-02-24 13:14:09 +0530

    sxs
    dss

--- workshop/index.php
+++ workshop/index.php
@@ -79,16 +79,12 @@ acids and sodium hydroxide solutions.</p>
                 </div>
                 <div class="col-md-4">
                     <div class="workshop">
-                      <h1>ASPEN PLUS</h1> 
+                      <h1>ANSYS FLUENT</h1> 
                         <hr>
-                        <p>ASPEN Plus is a process simulation software package widely used in
-industry today. Given a process design and an appropriate selection of
-thermodynamic models, ASPEN uses mathematical models to predict
-the performance of the process. This information can then be used in
-an iterative fashion to optimize the design. This accurate modelling of
-thermodynamic properties is particularly important in the separation
-of non-ideal mixtures, and ASPEN has a large database of regressed parameters. ASPEN can handle very complex processes, including multiple-column separation systems, chemical reactors, distillation of chemically reactive compounds and even electrolyte solutions like mineral
-acids and sodium hydroxide solutions.</p>
+                        <p>ANSYS Fluent is a simulation tool that predicts the impact of fluid flows
+throughout design and manufacturing as well as during end use. The
+software’s fluid flow analysis capabilities can be used to design and optimize new equipment and to troubleshoot already existing installations. The package can handle variety of fluid dynamics segments
+ranging from single- or multi-phase, isothermal or reacting, compressible or non-compressible.</p>
                     </div>
                 </div>
                 <div class="col-md-4">
