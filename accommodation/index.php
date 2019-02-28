7104cbc394bfd81d8de92f48487e853033e5a3d5
saicharan96 <saicharan14996@gmail.com>
2019-02-28 08:22:45 +0530

    s
    sss

--- accommodation/index.php
+++ accommodation/index.php
@@ -59,20 +59,225 @@
         </div>
     </nav>
     <br><br>
-          <div class="top evento">
-            <div class="container">
-            <div class="logos"><b style="font-family: 'Anton', sans-serif;font-style:20px;margin-top:-40px;">C<span>om</span>ing<span> S</span>oon !</b></div>
-                 <center> <div class="alert alert-info alert-dismissible">
-    <a href="#" class="close" data-dismiss="alert" aria-label="close" style="color:black !important;">&times;</a>
-   We are working Hard to provide you the safe and secured Accomodation until the event ends.
-  </div></center>
-                <br><br><br>
+                <section class="container">
+
+         <div class="shadow" >
+            <center> <div class="logos"><b style="font-family: 'Signika', sans-serif;font-style:20px;margin-top:-40px;">Acc<span>o</span>moda<span>tion</span></b></div>
+            </center>
+                 <br><br>
+             <h3>RULES :</h3>
+             <p>➢ Registration for accommodation is not part of the events
+registration, and is to be done separately.
+</p>
+             <p>➢ Hospitality services will be available from 11 march 2019 to 13
+march 2019</p>
+             <p>➢ Services will be offered on “First Register First Serve “basis.
+</p>
+             <p>➢ Accommodation will be provided on a sharing basis. Any
+requests for individual room will not be entertained.
+</p>
+             <p>➢ However, we will try to accommodate students from the same
+college (if possible)</p>
+             <p>➢ Participants are responsible for their belongings during their
+stay.</p>
+             <p>➢ In time is 9:00 pm for NRI hostels.</p>
+             <p>➢ Random checks will be made to avoid illegal stay.</p>
+             <p>➢ Smoking and Drinking is strictly prohibited inside the room.</p>
+             <p>➢ Any act of misbehaviour inside the campus will be severely
+dealt, leading to the immediate expulsion of the team (in
+question) from the campus and cancellation of registration.</p>
+             <p>➢ Any damage caused by the participants to the campus property
+will result in recovery of the amount due to the damage
+(whichever is higher)</p>
+             <p>➢ Accommodated students can have their food at their own
+expense in canteens and food stalls that are available.</p>
+             <br><br>    <center> <div class="logos"><b style="font-family: 'Signika', sans-serif;font-style:20px;margin-top:-40px;">Ab<span>o</span>ut<span> Rooms</span></b></div>
+            </center>
+             <div class="row">
+                 <div class="col-md-6 ">
+                     <div class="workshop">
+                         <h3>☝🏻CLASS 1</h3><br>
+             <center><div class="con"><img src="../assets/images/5star.png"></div></center><br><br>
+             <p><span> RS 450</span> per head per day</p>
+                               <p>High class OYO Hotel Galaxy Manor, West saidapet. </p>
+                         <p><span>Travel time:-</span> 5kms (15mins) from our campus.</p>
+                         <p><span>Perks :-</span> A/C Room, Free WiFi connection, 3days Compliment Breakfast TV
+facility.</p>
+                         <p>We will take care of the Transportation </p>
+                         <p><span>Total Inmates :-</span> 75 Members ( 3 per room ).</p>
+                         <p>Boys and Girls will be accommodated in <span>separate rooms.</span>
+</p>
+                         <p><span>PS :-</span> March 11th morning breakfast will be provided at the hotel for those
+alone who check in on 10th night. Check out time will be considered as the
+same as check in time.</p>
+                         <br><center><button class="button2" data-toggle="modal" data-target="#myModal-2">Avail For Accomodation</button></center>
         </div></div>
-        <footer class=""><center><a href="https://www.facebook.com/PetroVisionOfficial/" class="fa fa-facebook"></a>
-       <a href="#" class="fa fa-twitter"></a>
-       <a href="https://www.instagram.com/_petrovision2k19q" class="fa fa-instagram"></a>
-       </center>
-            <br><center><p>Copyrights &copy 2019 Petrovision. All Rights Reserved</p>
-        <span class="craft">Crafted By <a href="http://www.seotechie.tk/" target="_blank">SEOTechie</a></span></center></footer>
+             
+             
+              <div class="col-md-6 ">
+                     <div class="workshop">
+                         <h3>✌CLASS 2</h3><br>
+             <center><div class="con"><img src="../assets/images/hotel.png"></div></center><br><br>
+             <p><span> RS 350</span> per head per day.</p>
+                               <p>Non-resident Indian Hostel inside our Campus. </p>
+                         <p><span>Non-AC </span> room</p>
+                         <p><span>No food </span> will be provided</p>
+                         <p><span>Total Inmates :-</span> 60 Members ( 2 per room )</p>
+                         <p>Boys and Girls will be accommodated in <span>separate rooms</span>
+</p><p>Accommodation will be provided on first come first serve Basis</p>
+                 <br> <br> <br><br> <br><button class="button1" data-toggle="modal" data-target="#myModal-2">Avail For Accomodation</button>     
+        </div></div>
+             
+             </div>
+             <br><br>
+            </div>
+        </section>
+      <div class="modal fade" id="myModal-2" tabindex="-1" role="dialog" aria-labelledby="myModalLabel-2">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h4 class="modal-title" id="myModalLabel-2">Avail Accomodation Soon! </h4>
+      </div>
+      <div class="modal-body">
+        <center><p>Choose your suitable options according to your convenience</p>
+                  <div class="row">
+            <div class="col-md-6"><center><a href="/PaytmKit/TxnTest.php"><img src="../assets/images/debit-card%20(1).png"></a><br><p>Online</p></center></div>
+                  <div class="col-md-6"><center><a href="/success/"><img src="../assets/images/cash.png"></a><br><p>Onspot</p></center></div>
+                  </div>
+                  </center>
+      </div>
+
+      <div class="modal-footer">
+        <button type="button" class="btn btn-dialog" data-dismiss="modal">Cancel</button>
+      </div>
+    </div><!-- modal-content -->
+  </div><!-- modal-dialog -->
+  </div>
+       <footer class=""><center><a href="https://www.facebook.com/PetroVisionOfficial/" class="fa fa-facebook"></a>
+<a href="#" class="fa fa-twitter"></a>
+<a href="https://www.instagram.com/_petrovision2k19q" class="fa fa-instagram"></a>
+</center>
+     <br><center><p class="footerp">Copyrights &copy 2019 Petrovision. All Rights Reserved</p>
+        <span class="craft">Crafted By <a href="http://www.seotechie.tk/" target="_blank">SEOTechie</a></span></center>
+        </footer>
+        <style>.shadow {
+  position: relative;
+  margin: 120px auto 0;
+  padding:20px;
+  background: linear-gradient(0deg, #000, #262626);
+}
+            p
+            {
+                font-family: 'ProximaNovaSemibold';
+    color:#bbb;
+    font-size:20px;
+            }
+.shadow:before,
+.shadow:after {
+  content: "";
+  position: absolute;
+  top: -2px;
+  left: -2px;
+  background: linear-gradient(
+    45deg,
+    rgb(250,250,250),
+    rgb(0, 190, 200),
+    rgb(250,250,250)
+  );
+  background-size: 400%;
+  width: calc(100% + 4px);
+  height: calc(100% + 4px);
+  z-index: -1;
+  animation: moveBackground 20s linear infinite;
+}
+            button {
+  background: #4CAF50;
+  border: none;
+  border-radius: 25px;
+  color: snow; // Vanilla HTML color name
+  cursor: pointer;
+  font-size: 1.25em;
+  letter-spacing: .1em;
+  outline: none;
+  padding: .75em 2em;
+  position: relative; // Required for pseudo-element positioning
+  text-transform: uppercase;
+}
+.shadow:after {
+  filter: blur(20px);
+}
+@keyframes moveBackground{
+  0% {
+    background-position: 0 0;
+  }
+  50%{
+    background-position: 400% 0;
+  }
+  100% {
+    background-position: 0 0;
+  }
+}
+</style>
+         <style>
+.char {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  max-width: 300px;
+  margin: auto;
+  background-color:#ffffff;
+  text-align: center;
+  font-family: arial;
+}
+
+.title {
+  color: grey;
+  font-size: 18px;
+}
+.char h1
+         {
+             font-family: 'ProximaNovaSemibold';
+    text-align: center;
+    font-size:30px;
+         }
+         .workshop p
+         {
+             font-family: 'ProximaNovaSemibold';
+    font-size:20px;
+         }
+button {
+  border: none;
+  outline: 0;
+  display: inline-block;
+  padding: 8px;
+  color: black;
+  background-color: #FCBB1C;
+  text-align: center;
+  cursor: pointer;
+  width: 100%;
+  font-size: 18px;
+}
+
+a {
+  text-decoration: none;
+  font-size: 22px;
+  color: black;
+}
+             .workshop p span
+             {
+                 color:red;
+             }
+
+             .workshop p
+             {
+                 color:#000000;
+             }
+             .workshop h3
+             {
+                 color:#000000;
+             }
+button:hover, a:hover {
+  opacity: 0.7;
+}
+</style>
+
 
     </body></html>
