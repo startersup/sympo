a27ce559df4b00b3cbe182b9e8889d73ae83d4b2
vinodhan16 <vinodhan.thunderbolt@gmail.com>
2019-03-01 20:36:26 +0530

    Update test.php

--- accommodation/test.php
+++ accommodation/test.php
@@ -52,7 +52,7 @@ $headers .= "Content-type:text/html;charset=UTF-8" . "\r\b";
                 <div class="mktEditable" id="main_text">
                   Hello,<br>
                   Greetings from Petrovision!!<br>
-We, the organisers of Petrovision 2k19 would like to welcome you to our Extravaganza of this academic year. This mail is in confirmation of your successful registration for the acommodation. You have opted '$class' Accommodation. We expect to meet you soon and deliver the best staying experience for you . Once again, a warm welcome from the organising committee of Petrovision 2k19.<br>
+We, the organisers of Petrovision 2k19 would like to welcome you to our Extravaganza of this academic year. This mail is in confirmation of your successful registration for the acommodation. You have opted '.$class.'. Accommodation. We expect to meet you soon and deliver the best staying experience for you . Once again, a warm welcome from the organising committee of Petrovision 2k19.<br>
                 </div>
                 </td>
               </tr>
@@ -144,3 +144,30 @@ mail($to, $subject, $message, $headers);
   exit();
 }
  ?>
+
+ <html lang="en">
+ <head>
+ <meta charset="utf-8" />
+ <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+ <meta name="viewport" content="width=device-width, initial-scale=1">
+ <link href="https://fonts.googleapis.com/css?family=Lato:300,400|Montserrat:700" rel="stylesheet" type="text/css">
+ <style>
+ @import url(//cdnjs.cloudflare.com/ajax/libs/normalize/3.0.1/normalize.min.css);
+ @import url(//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css);
+ </style>
+ <link rel="stylesheet" href="https://2-22-4-dot-lead-pages.appspot.com/static/lp918/min/default_thank_you.css">
+ <script src="https://2-22-4-dot-lead-pages.appspot.com/static/lp918/min/jquery-1.9.1.min.js"></script>
+ <script src="https://2-22-4-dot-lead-pages.appspot.com/static/lp918/min/html5shiv.js"></script>
+ </head>
+ <body>
+ <header class="site-header" id="header">
+ <h2 class="site-header__title" data-lead-id="site-header-title">Success!</h2>
+ </header>
+
+ <div class="main-content">
+ <i class="fa fa-check main-content__checkmark" id="checkmark"></i>
+ <p class="main-content__body" data-lead-id="main-content-body">You have successfully subscribed the event.You have chosen onspot resgistration.</p>
+ </div>
+ <center><a href="http://petrovision.co.in">Back to Home</a></center>
+ </body>
+ </html>
