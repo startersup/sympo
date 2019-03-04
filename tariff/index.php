0a1ea9feae6bdb86d0a8c2caf43588e46e3d25ab
vinodhan16 <vinodhan.thunderbolt@gmail.com>
2019-03-04 12:30:14 +0530

    Update index.php

--- tariff/index.php
+++ tariff/index.php
@@ -11,14 +11,14 @@ session_start();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://fonts.googleapis.com/css?family=Merriweather|Open+Sans|Roboto:500" rel="stylesheet">
-    <link rel="stylesheet" href="../assets/css/style.css">
+  
     <script src="../assets/js/sidenav.js"></script>
     <link href="https://fonts.googleapis.com/css?family=Archivo+Black|Asap|Bangers|Carter+One|Concert+One|Francois+One|Kanit|Merienda|Noto+Sans+TC|Anton|Passion+One|Signika|Vollkorn" rel="stylesheet">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="../assets/css/fonts.css">
+
     <script src="../assets/js/sidenav.js"></script>
 </head>
 
