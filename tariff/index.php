a2777224a1a9cd8d488481323c3373ce43df4cc4
vinodhan16 <vinodhan.thunderbolt@gmail.com>
2019-03-04 12:31:22 +0530

    Update index.php

--- tariff/index.php
+++ tariff/index.php
@@ -18,7 +18,7 @@ session_start();
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
+    <link rel="stylesheet" href="../assets/css/fonts.css">
     <script src="../assets/js/sidenav.js"></script>
 </head>
 
